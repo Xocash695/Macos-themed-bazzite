b15d1dc7678b217f45bf848d9dadf94b2063c0ae
Akash Kallumkal <akash.kallumkal@gmail.com>
2026-06-07 17:35:22 -0400

    pretty please work

--- build_files/build.sh
+++ build_files/build.sh
@@ -152,9 +152,8 @@ sed -i 's/gpgcheck=1/gpgcheck=0/g; /gpgkey=file:\/\//d' /etc/yum.repos.d/terra-m
 # ==============================================================================
 # 9. DEFAULT USER PROFILE CONFIGURATIONS (konsave)
 # ==============================================================================
-mkdir -p /opt/konsave
-pip3 install konsave --break-system-packages --target=/opt/konsave
-export PYTHONPATH="/opt/konsave:$PYTHONPATH"
+pip3 install konsave --break-system-packages --target=/var/opt/konsave
+export PYTHONPATH="/var/opt/konsave:$PYTHONPATH"
 konsave -i /ctx/macOS-layout.knsv
 konsave -a macOS-layout -o /etc/skel
 
